3032c8f3457e4fe2bf9db7f488cc61286aa7d71c
idilunver <unveridil@gmail.com>
2026-08-02 01:24:14 +0300

    Built web
    Branch: main
    Commit: cb99bd5b68625ce41ee02470d1798b4c163a0326
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3143738046"
+    serviceWorkerVersion: "2249261027"
   }
 });
